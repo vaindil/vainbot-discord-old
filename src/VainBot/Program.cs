4d6635c9596268d41024cbf0cc1368bf705328be
Steven Hall <vaindil@gmail.com>
2017-01-14 22:21:41 -0500

    it works

--- src/VainBot/Program.cs
+++ src/VainBot/Program.cs
@@ -1,14 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Discord;
+using Discord.WebSocket;
 using System.Threading.Tasks;
 
 namespace VainBot
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args) => new Program().Run().GetAwaiter().GetResult();
+
+        DiscordSocketClient client;
+
+        public async Task Run()
         {
+            client = new DiscordSocketClient();
+
+            var token = "MTYyMzU2Mzk4ODU0ODk3NjY0.C1x6Ug.vjYR0YEXsi40NtVfi6QnDCl7DYE";
+
+            client.MessageReceived += async (message) =>
+            {
+                if (message.Content == "!ping")
+                    await message.Channel.SendMessageAsync("pong");
+            };
+
+            await client.LoginAsync(TokenType.Bot, token);
+
+            await client.ConnectAsync();
+
+            await Task.Delay(-1);
         }
     }
 }
