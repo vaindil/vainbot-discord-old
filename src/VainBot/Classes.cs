d784dc5b3151b51e8a20e2086e836a079f1d8c86
Steven Hall <vaindil@gmail.com>
2017-02-03 22:15:24 -0500

    add twitch ID command

--- src/VainBot/Classes.cs
+++ src/VainBot/Classes.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 
 namespace VainBot
 {
@@ -32,9 +34,38 @@ namespace VainBot
         public List<string> Facts { get; set; }
     }
 
+    public class TwitchUserResponse
+    {
+        [JsonProperty(PropertyName = "_total")]
+        public int Total { get; set; }
+
+        public List<TwitchUser> Users { get; set; }
+    }
+
+    public class TwitchUser
+    {
+        [JsonProperty(PropertyName = "_id")]
+        public long Id { get; set; }
+
+        public string Type { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public string Logo { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Bio { get; set; }
+    }
+
     public enum DbKey
     {
         DiscordApiKey,
+        TwitchClientId,
         CrendorServerId,
         PointsNameSingular,
         PointsNamePlural,
---
+++ src/VainBot/Commands/TwitchIdModule.cs
@@ -0,0 +1,65 @@
+﻿using Discord.Commands;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace VainBot.Commands
+{
+    [Group("twitchid")]
+    public class TwitchIdModule : ModuleBase
+    {
+        readonly VbContext _context;
+        readonly HttpClient _client;
+        
+        public TwitchIdModule(VbContext context, HttpClient client)
+        {
+            _context = context;
+            _client = client;
+        }
+
+        [Command]
+        public async Task GetId(string username)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                await ReplyAsync("That username is empty.");
+                return;
+            }
+
+            if (username.Length > 20)
+            {
+                await ReplyAsync("No Twitch username is that long.");
+                return;
+            }
+
+            var clientId = await _context.KeyValues.GetValueAsync(DbKey.TwitchClientId);
+
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                "https://api.twitch.tv/kraken/users?login=" + username);
+
+            request.Headers.Accept.Clear();
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.twitchtv.v5+json"));
+            request.Headers.Add("Client-ID", clientId);
+
+            var response = await _client.SendAsync(request);
+            if (!response.IsSuccessStatusCode)
+            {
+                await ReplyAsync("Error calling the Twitch API. Sucks to be you.");
+                return;
+            }
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            var twitchUser = JsonConvert.DeserializeObject<TwitchUserResponse>(responseContent, Extensions.GetJsonSettings());
+            if (twitchUser == null || twitchUser.Users == null || twitchUser.Users.Count != 1)
+            {
+                await ReplyAsync("The Twitch response isn't what I was expecting. Go bug vaindil about it.");
+                return;
+            }
+
+            await ReplyAsync($"{twitchUser.Users[0].DisplayName}: ID is {twitchUser.Users[0].Id}");
+        }
+    }
+}
