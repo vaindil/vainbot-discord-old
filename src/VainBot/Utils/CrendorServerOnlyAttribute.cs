a8f3570a991822ad5116b7451e8c8932e72c6d70
Steven Hall <vaindil@gmail.com>
2017-03-10 19:18:14 -0500

    suppress this because it annoys me

--- src/VainBot/Utils/CrendorServerOnlyAttribute.cs
+++ src/VainBot/Utils/CrendorServerOnlyAttribute.cs
@@ -5,7 +5,9 @@ namespace VainBotDiscord.Utils
 {
     public class CrendorServerOnlyAttribute : PreconditionAttribute
     {
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public async override Task<PreconditionResult> CheckPermissions(ICommandContext context,
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
                                                                         CommandInfo command,
                                                                         IDependencyMap map)
         {
