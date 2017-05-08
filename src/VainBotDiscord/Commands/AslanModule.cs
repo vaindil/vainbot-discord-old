4932c0e990b4a6ad77752a9c6b090e1529bcda4e
Steven Hall <vaindil@gmail.com>
2017-05-08 09:43:30 -0400

    bot no longer bullies baudgee, update info message

--- src/VainBotDiscord/Commands/AslanModule.cs
+++ src/VainBotDiscord/Commands/AslanModule.cs
@@ -11,8 +11,8 @@ namespace VainBotDiscord.Commands
         public async Task BotInfo([Remainder]string unused = null)
         {
             await ReplyAsync("<:ASLAN:271856531505545236> I post in <#291758537808412690> when " +
-                "Destiny goes live on Twitch, posts a new YouTube video, or tweets. I also bully Baudgee. " +
-                "<:FrankerZ:271856531904004126> Pester vaindil with any issues (vaindil#2314 on Discord). " +
+                "Destiny goes live on Twitch, posts a new YouTube video, or tweets. " +
+                "Pester vaindil with any issues (vaindil#2314 on Discord). " +
                 "Source is on GitHub: <https://github.com/vaindil/vainbot-discord>");
         }
 
