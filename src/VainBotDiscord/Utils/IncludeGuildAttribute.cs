1fa2fb05598e91dc8a33cf243d892440e39b8b1a
Steven Hall <vaindil@gmail.com>
2017-05-11 09:39:34 -0400

    [untested] add include/exclude guild preconditions

---
+++ src/VainBotDiscord/Utils/IncludeGuildAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Discord.Commands;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VainBotDiscord.Utils
+{
+    public class IncludeGuildAttribute : ParameterPreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissions(ICommandContext context,
+                                                                  ParameterInfo parameter,
+                                                                  object value,
+                                                                  IServiceProvider services)
+        {
+            var guildList = value as ulong[];
+            if (guildList.Contains(context.Guild.Id))
+                return Task.FromResult(PreconditionResult.FromSuccess());
+
+            return Task.FromResult(PreconditionResult.FromError("Command is prohibited in this guild."));
+        }
+    }
+}
