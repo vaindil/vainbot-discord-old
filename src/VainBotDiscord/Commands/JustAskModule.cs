a8e97d873585d74556e8395e03d334d883cfd1cf
Steven Hall <vaindil@gmail.com>
2017-06-13 01:32:24 -0400

    add justask

---
+++ src/VainBotDiscord/Commands/JustAskModule.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace VainBotDiscord.Commands
+{
+    public class JustAskModule : ModuleBase
+    {
+        [Command("justask")]
+        [Alias("ask")]
+        public async Task JustAsk([Remainder]string unused = null)
+        {
+            await ReplyAsync("Just ask your question and someone will help if they can.");
+        }
+    }
+}
