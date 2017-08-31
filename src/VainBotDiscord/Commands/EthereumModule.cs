4de6b0939552f57e4c7c16e7435e82b9ea6fcb30
Steven Hall <vaindil@gmail.com>
2017-08-31 11:42:13 -0400

    remove Cake's stuff

--- src/VainBotDiscord/Commands/EthereumModule.cs
+++ src/VainBotDiscord/Commands/EthereumModule.cs
@@ -1,7 +1,5 @@
 ﻿using Discord.Commands;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +15,6 @@ namespace VainBotDiscord.Commands
         [Command]
         public async Task Eth([Remainder]string unused = null)
         {
-            const decimal cakeStartEth = 352.44M;
-            const decimal cakeStartBtc = 2740.24M;
-
             var priceString = await _httpClient
                 .GetStringAsync("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD");
             var prices = JsonConvert.DeserializeObject<CryptoApiResponse>(priceString);
@@ -31,13 +26,6 @@ namespace VainBotDiscord.Commands
 
             sb.Append("Current BTC price: ");
             sb.Append(prices.Btc.Usd);
-
-            if (Context.Guild.Id == 268547141721522178)
-            {
-                sb.Append("\n\n__Cake's starting prices__ _(2017-06-21 0100)_\n");
-                sb.Append($"ETH: {cakeStartEth} | {prices.Eth.Usd - cakeStartEth}\n");
-                sb.Append($"BTC: {cakeStartBtc} | {prices.Btc.Usd - cakeStartBtc}");
-            }
 
             await ReplyAsync(sb.ToString());
         }
